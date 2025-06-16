a9205db52b53c7118870c2e29f6a2acb91ae017d
Sumit Roy <sroy8091@gmail.com>
2025-06-16 08:04:51 +0530

    codecrafters submit [skip ci]

--- app/commands.py
+++ app/commands.py
@@ -1,3 +1,5 @@
+import json
+
 from app.RESPCodec import RESPDecoder, RESPEncoder
 
 
@@ -9,11 +11,23 @@ def handle_input(data):
     if "PING" in ins:
         command, args = "PING", None
     else:
-        command, args = ins
+        command, *args = ins
     return encoder.encode(resolver[command](args))
 
+def set_memory(data):
+    json_str = json.dumps({data[0]: data[1]})
+    with open('memory.json', 'w') as f:
+        f.write(json_str)
+    return "OK"
+
+def get_memory(data):
+    with open('memory.json', 'r') as f:
+        memory = json.loads(f.read())
+    return memory[data[0]]
 
 resolver = {
     "PING": lambda x : "PONG",
-    "ECHO": lambda x : x
+    "ECHO": lambda x : x[0],
+    "SET": set_memory,
+    "GET": get_memory,
 }

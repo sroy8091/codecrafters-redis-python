1470e5b7c2f63b222ad127d300e67bce036a9af2
Sumit Roy <sroy8091@gmail.com>
2025-07-15 23:31:18 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ import argparse
 from app.memory import RedisStore
 from app.server import ServerHandler
 
-async def main(storage):
+async def main(storage, port):
     handler = ServerHandler(storage)
-    server = await asyncio.start_server(handler.respond, "localhost", 6379)
+    server = await asyncio.start_server(handler.respond, "localhost", port)
     addrs = ', '.join(str(sock.getsockname()) for sock in server.sockets)
     print(f'Serving on {addrs}')
 
@@ -19,7 +19,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Redis server implementation')
     parser.add_argument('--dir', type=str, help='the path to the directory where the RDB file is stored')
     parser.add_argument('--dbfilename', type=str, help='the name of the RDB file')
+    parser.add_argument('--port', type=int, default=6379, help='the port to run the server on')
     args = parser.parse_args()
     storage = RedisStore(args.dir, args.dbfilename)
 
-    asyncio.run(main(storage))
+    asyncio.run(main(storage, args.port))
